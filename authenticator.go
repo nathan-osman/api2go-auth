e4bac79c422c23ebd77c496eef1159fb8cc7b64e
Nathan Osman <nathan@quickmediasolutions.com>
2018-04-05 12:28:21 -0700

    Update documentation.

--- authenticator.go
+++ authenticator.go
@@ -7,13 +7,15 @@ type Authenticator interface {
 
 	// Authenticate determines if the request represents a valid login attempt.
 	// The first return value is a unique identifier stored with the session
-	// and used for initializing requests. The second return value is encoded
-	// as JSON and returned to the client. The third return value is used if an
-	// error occurs during authentication (a bad password, for example).
+	// and used for initializing requests. This value should be set to nil if
+	// the provided credentials are invalid or insufficient. The second return
+	// value is encoded as JSON and returned to the client. The third return
+	// value is used if an error occurs during authentication.
 	Authenticate(r *http.Request) (interface{}, interface{}, error)
 
 	// Initialize prepares an authenticated request for processing. Typically,
 	// this involves setting a value on the request's context based on the
-	// provided session object (returned by Authenticate).
+	// provided session object (returned by Authenticate). The request is not
+	// processed further if an error is returned.
 	Initialize(r *http.Request, i interface{}) error
 }
--- login.go
+++ login.go
@@ -1,13 +1,20 @@
 package auth
 
 import (
+	"errors"
 	"net/http"
 )
+
+var errAuthFailed = errors.New("authentication failed")
 
 func (a *Auth) login(w http.ResponseWriter, r *http.Request) {
 	id, i, err := a.authenticator.Authenticate(r)
 	if err != nil {
-		writeError(w, err, http.StatusForbidden)
+		writeError(w, err, http.StatusInternalServerError)
+		return
+	}
+	if id == nil {
+		writeError(w, errAuthFailed, http.StatusForbidden)
 		return
 	}
 	session, _ := a.store.Get(r, sessionName)
