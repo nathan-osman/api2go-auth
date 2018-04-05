ec55e211220fd914af9222ba9343f47fb5becf7c
Nathan Osman <nathan@quickmediasolutions.com>
2018-04-05 10:51:58 -0700

    Update Auth type.

--- auth.go
+++ auth.go
@@ -4,41 +4,35 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"github.com/manyminds/api2go"
 )
 
-// Authenticator provides methods for authenticating requests.
-type Authenticator interface {
-
-	// Authenticate determines if the request represents a valid login attempt
-	// and returns an object to associate with the session or an error if the
-	// login attempt is invalid.
-	Authenticate(r *http.Request) (interface{}, error)
-
-	// Initialize prepares an authenticated request for processing. Typically,
-	// this involves setting a value on the request's context based on the
-	// provided session object (set by Authenticate).
-	Initialize(r *http.Request, i interface{})
-}
+const (
+	sessionName = "name"
+	sessionData = "data"
+)
 
 // Auth provides a handler to authenticate requests. In addition to serving the
 // routes provided by the API, two additional routes are added for logging in
 // and logging out.
 type Auth struct {
-	api    *api2go.API
-	a11r   Authenticator
-	router *mux.Router
+	api           *api2go.API
+	authenticator Authenticator
+	router        *mux.Router
+	store         *sessions.CookieStore
 }
 
 // New creates a new handler for the provided API using the provided
 // authenticator for requests.
-func New(api *api2go.API, a11r Authenticator) *Auth {
+func New(api *api2go.API, authenticator Authenticator, secretKey string) *Auth {
 	var (
 		r = mux.NewRouter()
 		a = &Auth{
-			api:    api,
-			a11r:   a11r,
-			router: r,
+			api:           api,
+			authenticator: authenticator,
+			router:        r,
+			store:         sessions.NewCookieStore([]byte(secretKey)),
 		}
 	)
 	r.HandleFunc("/login", a.login)
@@ -49,5 +43,5 @@ func New(api *api2go.API, a11r Authenticator) *Auth {
 
 // ServeHTTP responds to requests for API resources.
 func (a *Auth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	a.router.ServeHTTP(w, r)
 }
